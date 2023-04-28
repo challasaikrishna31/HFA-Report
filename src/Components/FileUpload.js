c5d5945e94f44496ae7b8b10f932d2bda78d993f
skchalla <skchalla@DESKTOP-NU9V6HJ>
2023-04-28 14:58:24 +0530

    updated file upload logic

--- src/Components/FileUpload.js
+++ src/Components/FileUpload.js
@@ -1,41 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import axios from "axios";
+import { useDropzone } from "react-dropzone";
+import { FiUpload } from "react-icons/fi";
+import "tailwindcss/tailwind.css";
+import { uploadFile } from "../Services/ReportService";
 
-function FileUpload() {
-    const [selectedFile, setSelectedFile] = useState(null);
-    const [message, setMessage] = useState('');
+const UploadFile = () => {
+    const [file, setFile] = useState(null);
+    const [fileName, setFileName] = useState("Choose file");
 
-    const handleFileInput = (e) => {
-        setSelectedFile(e.target.files[0]);
-    };
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        accept: "image/*",
+        onDrop: (acceptedFiles) => {
+            setFile(acceptedFiles[0]);
+            setFileName(acceptedFiles[0].name);
+        },
+    });
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const formData = new FormData();
-        formData.append('file', selectedFile);
-
-        fetch('https://example-api.com/upload', {
-            method: 'POST',
-            body: formData
+        formData.append("file", file);
+        uploadFile(formData).then((resp) => {
+            console.log(resp);
         })
-            .then(response => response.json())
-            .then(data => {
-                setMessage(data.message);
-            })
-            .catch(error => {
-                setMessage('Error uploading file.');
-                console.error(error);
-            });
+        // try {
+        //     const res = await axios.post("https://your-api-endpoint.com/upload", formData);
+        //     console.log(res.data);
+        // } catch (err) {
+        //     console.error(err);
+        // }
     };
-
+    const clearFiles = () => {
+        setFile(null);
+    }
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input type="file" onChange={handleFileInput} multiple />
-                <button type="submit">Upload</button>
-            </form>
-            {message && <p>{message}</p>}
+        <div className="flex flex-col items-center justify-center m-5">
+            <div
+                {...getRootProps()}
+                className="w-full max-w-md p-8 mx-auto border-2 border-dashed rounded-lg cursor-pointer transition-all duration-300 ease-in-out hover:border-gray-500"
+            >
+                <input {...getInputProps()} />
+                {isDragActive ? (
+                    <p className="text-2xl font-semibold text-gray-500">Drop your file here</p>
+                ) : (
+                    <p className="text-2xl font-semibold text-gray-500">
+                        <FiUpload className="inline-block mb-1 mr-2 text-gray-400" />
+                        Drag &amp; drop or{" "}
+                        <span className="underline text-blue-500 hover:text-blue-700">
+                            browse
+                        </span>{" "}
+                        your file here
+                    </p>
+                )}
+                <p className="mt-2 text-sm text-gray-500">{fileName}</p>
+            </div>
+            {file && (
+                <div>
+                    <button
+                        onClick={handleSubmit}
+                        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                    >
+                        Upload
+                    </button>
+                    <button
+                        onClick={clearFiles}
+                        className="mt-4 mx-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                    >
+                        Clear
+                    </button>
+                </div>
+            )}
         </div>
     );
-}
+};
 
-export default FileUpload;
+export default UploadFile;
--- src/Services/ReportService.js
+++ src/Services/ReportService.js
@@ -1,9 +1,23 @@
+import axios from "axios";
 export async function getReportData() {
     try {
-        const response = await fetch('http://localhost:8000/hfareport');
-        return await response.json();
+        // const response = await fetch('http://localhost:8000/hfareport');
+        const response = await axios(
+            'http://localhost:8000/hfareport'
+        );
+        return response.data;
     } catch (error) {
         return [];
+    }
+
+}
+
+export async function uploadFile(formData) {
+    try {
+        const response = await axios.post("https://your-api-endpoint.com/upload", formData);
+        return response.data;
+    } catch (err) {
+        console.error(err);
     }
 
 }
